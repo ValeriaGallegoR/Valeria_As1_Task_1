1c18034d42ddd29de9494776ddeb5fd3a8111339
ValeriaGallegoR <valeriagallego20@gmail.com>
2025-01-29 20:21:28 -0500

    Add class Task1 and declaration of roundtrip cost variables

--- Program.cs
+++ Program.cs
@@ -1,1 +1,15 @@
 ﻿//PROG-8051 Assignment-1
+
+namespace Valeria_As1_Task_1 {
+
+    class Task1
+    {
+        static void Main(string[] args)
+        {
+            //Declaration of roundtrip cost variables
+            double YyzToCgy = 1350 * 2;
+            double YyzToVan = 1500 * 2;
+            double YyzToYmq = 575 * 2;
+        }
+    }
+}
